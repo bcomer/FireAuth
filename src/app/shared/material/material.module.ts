11cd4af2e85fe20e452e079013f67c6bf475e5f7
Brandon Comer <bcomer@hoganassessments.com>
2018-11-14 14:03:16 -0600

    added angular material

---
+++ src/app/shared/material/material.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatButtonModule, MatInputModule} from '@angular/material';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatInputModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatInputModule
+  ]
+})
+export class MaterialModule { }
